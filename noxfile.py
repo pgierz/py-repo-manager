f9f7440e9cf9b8476d4601908dc54aad67ee7227
Paul Gierz <pgierz@mac.com>
2023-12-21 19:39:50 -0800

    wip: cli is on the right track, should fail in CI, too

--- noxfile.py
+++ noxfile.py
@@ -3,11 +3,13 @@ import nox
 
 @nox.session
 def tests(session):
-    session.install("pytest")
-    session.run("pytest")
+    """Run the test suite."""
+    tests = session.posargs or ["tests/"]
+    env = {"VIRTUAL_ENV": session.virtualenv.location}
+    session.run("pipenv", "install", "--dev", external=True, env=env)
+    session.run("pytest", *tests, env=env)
 
 
 @nox.session
 def lint(session):
-    session.install("flake8")
     session.run("flake8")
